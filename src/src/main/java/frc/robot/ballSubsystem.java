b84a309107949afcdb1c28c9001500a9bcd8b04a
Dawson Schraiber <dschraiber1@gmail.com>
2020-02-10 22:13:59 -0600

    Initial System Foundations/Skeleton for Controls

---
+++ src/src/main/java/frc/robot/ballSubsystem.java
@@ -0,0 +1,45 @@
+package ballSubsystem;
+
+public static class intakeSubsystem {
+
+    public void intakeOn() {
+
+    }
+
+    public void intakeOff() {
+
+    }
+
+    public void intakeExtend() {
+    /* separate system */
+
+    }
+
+    public void intakeRetract() {
+    /* separate system */
+    
+    }
+}
+
+public static class hopperSubsystem {
+
+    public void hopperOn() {
+
+    }
+
+    public void hopperOff() {
+
+    }
+
+}
+
+public static class aimSubsystem {
+
+    public void autoAimOn() {
+
+    }
+
+    public void autoAimOff() {
+        
+    }
+}
