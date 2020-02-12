b14f4b2d8f74b1f41c3067067955f61bb0d40aa7
Kevin Gray <graykevinb@gmail.com>
2020-02-11 19:24:25 -0600

    Added constants for axis and button ids

--- src/src/main/java/frc/robot/Axis.java
+++ src/src/main/java/frc/robot/Axis.java
@@ -6,6 +6,14 @@ public class Axis {
     private int axis_id;
     private Joystick axisController;
 
+    //Axis IDs
+    public static final int LEFT_STICK_X = 0;
+    public static final int LEFT_STICK_Y = 1;
+    public static final int LEFT_TRIGGER = 2;
+    public static final int RIGHT_TRIGGER = 3;
+    public static final int RIGHT_STICK_X = 4;
+    public static final int RIGHT_STICK_Y = 5;
+
     public Axis(int controller_id, int axis_id) {
         this.controller_id = controller_id;
         this.axis_id = axis_id;
--- src/src/main/java/frc/robot/Button.java
+++ src/src/main/java/frc/robot/Button.java
@@ -6,6 +6,17 @@ public class Button {
     private int controller_id;
     private int axis_id;
     private Joystick axisController;
+    
+    //Button IDs
+    public static final int A = 1;
+    public static final int B = 2;
+    public static final int X = 3;
+    public static final int Y = 4;
+    public static final int LEFT_BUMPER = 5;
+    public static final int RIGHT_BUMPER = 6;
+    public static final int BACK = 7;
+    public static final int START = 8;
+    
 
     public Button(int controller_id, int axis_id) {
         this.controller_id = controller_id;
