2f3438328fdc3342c22bc83c03cc5c4ee09ef718
Kevin Gray <graykevinb@gmail.com>
2020-03-04 18:56:34 -0600

    Added class of talon id constants

---
+++ src/src/main/java/frc/robot/HardwareIDs.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+public class HardwareIDs {
+    public final static int INTAKE = 1;
+    public final static int FIRST_FEEDER = 2;
+    public final static int SECOND_FEEDER = 3;
+    public final static int QUEING = 4;
+    public final static int TURRET = 5;
+    public final static int RIGHT_SHOOTER = 6;
+    public final static int LEFT_SHOOTER = 7;
+}
